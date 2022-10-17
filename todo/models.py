5c6897aea35544e1a5ddff60db16976902427090
Alex <alexzalox@gmail.com>
2022-10-17 21:46:42 +0800

    完成todo資料表建立

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Todo(models.Model):
+    title=models.CharField(max_length=100)
+    text=models.TextField(blank=True)
+    created=models.DateTimeField(auto_now_add=True)
+    date_completed=models.DateTimeField(blank=True,null=True)
+    important=models.BooleanField(default=False)
+
+
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
